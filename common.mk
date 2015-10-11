e442c921012b99d6863a3891fa1cb4d43574794a
Markus Schraeder <gitlab@priv.de>
2015-10-12 00:54:53 +0200

    Fetch git from git.priv.de not github

--- common.mk
+++ common.mk
@@ -15,7 +15,7 @@ UBOOT_TOOLCHAIN := gcc-linaro-arm-none-eabi-4.8-2014.04_linux.tar.xz
 UBOOT_TOOLCHAIN_URL := http://www.priv.de/odroid/$(UBOOT_TOOLCHAIN)
 UBOOT_TC_DIR := uboot_tc
 UBOOT_TC_PATH := $(UBOOT_TC_DIR)/gcc-linaro-arm-none-eabi-4.8-2014.04_linux/bin
-UBOOT_REPO := https://github.com/pRiVi/u-boot.git
+UBOOT_REPO := https://git.priv.de/odroid/u-boot.git
 UBOOT_BRANCH := odroidc-v2011.03
 UBOOT_SRC := u-boot
 
@@ -24,7 +24,7 @@ LINUX_TOOLCHAIN_URL := http://www.priv.de/odroid/$(LINUX_TOOLCHAIN)
 LINUX_TC_DIR := linux_tc
 LINUX_TC_PATH := $(LINUX_TC_DIR)/bin
 LINUX_TC_PREFIX := arm-linux-gnueabihf-
-LINUX_REPO := https://github.com/pRiVi/linux.git
+LINUX_REPO := https://git.priv.de/odroid/kernel.git
 LINUX_BRANCH := odroidc-3.10.y-lcdfix
 LINUX_SRC := linux
 
